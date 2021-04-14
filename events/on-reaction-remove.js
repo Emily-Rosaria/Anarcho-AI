7e72035cd84e8ceede1be803155f1761b912ff68
Emily Rose <ebonywoodhellscythe@gmail.com>
2021-04-14 23:48:16 +0100

    remove starboard

--- events/on-reaction-remove.js
+++ events/on-reaction-remove.js
@@ -1,6 +1,5 @@
 const config = require('./../config.json'); // load bot config
 const mongoose = require("mongoose"); //database library
-const Starboard = require("./../database/models/starboard.js"); // messages model
 
 module.exports = {
   name: "onReactionRemove",

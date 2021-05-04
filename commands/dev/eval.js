1a6db8f06c43c102f35033ed5385db67d75e00e4
Emily Rose <ebonywoodhellscythe@gmail.com>
2021-05-04 04:19:20 +0100

    Add eval to test stuff. Devs only.

---
+++ commands/dev/eval.js
@@ -0,0 +1,29 @@
+const config = require('./../../config.json'); // load bot config
+const Discord = require('discord.js'); // Image embed
+
+module.exports = {
+    name: 'eval', // The name of the command
+    description: 'Runs code if user is a dev. Only takes in the message sent as an argument.', // The description of the command (for help text)
+    perms: 'dev',
+    allowDM: true,
+    usage: '<code>', // Help text to explain how to use the command (if it had any arguments)
+    args: true,
+    execute(message, args) {
+      if (message.author.id != config.dev) {
+        return;
+      }
+      const chunks = message.content.split('```');
+      if (chunks.length < 2) {
+        return message.reply("Invalid input.");
+      }
+      const code = message.content.split('```')[1].replace('^.*\n','');
+      var func = new Function(code);
+      try {
+        func();
+        message.reply("Done!");
+      } catch (err) {
+        const error = err.stack.length > 1955 ? err.stack.substring(0,1950) : err.stack;
+        message.reply("```\n"+err.stack+"\n```");
+      }
+    },
+};
